fdb6a2573b8c5fb3e594abdc6f4c3bdabae94819
hide-teck <hidehis111@gmail.com>
2023-03-04 09:14:03 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,35 @@
 import { NextPage } from 'next';
+import { GetStaticProps } from 'next';
+import fetch from 'node-fetch';
+import React from 'react';
 
-import Layout from '@/components/common/Layout';
-import Counter from '@/components/counter/Counter';
-import { getRandomNum } from '@/lib/counterUtil';
-
-const Home: NextPage = () => {
-  const initialNum = getRandomNum();
-
-  return (
-    <Layout path="/" title="タイトル" noTitleTemplate isTopPage>
-      <Counter initialNum={initialNum} />
-    </Layout>
-  );
+type Props = {
+  contentHtml: string;
 };
 
+const Home: NextPage<Props> = ({ contentHtml }) => {
+  // const changeUrlHtmlData = contentHtml.replace(
+  //   new RegExp(`https://effil.net`, 'g'),
+  //   'localhost:3333',
+  // );
+
+  return <div dangerouslySetInnerHTML={{ __html: contentHtml }} />;
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const response = await fetch('https://effil.net/puroguram/post-260/?_embed');
+  const html = await response.text();
+  return {
+    props: {
+      contentHtml: html,
+    },
+  };
+  // const response = await fetch('http://localhost:3333/api/wp');
+  // const { contentHtml } = (await response.json()) as { contentHtml: string };
+  // return {
+  //   props: {
+  //     contentHtml,
+  //   },
+  // };
+};
 export default Home;
